a2bbc3ff584d99ac194cdc7e065451ef1a47f3fb
Lázaro Vieira <lazaro-e-nemo@hotmail.com>
2017-04-05 06:34:32 -0300

    Fix typo

--- source/main.c
+++ source/main.c
@@ -12,15 +12,12 @@ int main() {
     u8 testmenuID[] = {0x02, 0x81, 0x00, 0x00, 0x30, 0x00, 0x04, 0x00};
 
     CFG_GetConfigInfoBlk4(0x8, 0x00110001, titleID);
-    bool isHomeMenu = !memcmp(titleID, testmenuID, 0x8);
+    bool isTestMenu = !memcmp(titleID, testmenuID, 0x8);
 
-    if (isHomeMenu) CFG_SetConfigInfoBlk8(0x8, 0x00110001, homemenuID);
+    if (isTestMenu) CFG_SetConfigInfoBlk8(0x8, 0x00110001, homemenuID);
     else CFG_SetConfigInfoBlk8(0x8, 0x00110001, testmenuID);
 
     CFG_UpdateConfigNANDSavegame();
-    // CFG_GetConfigInfoBlk4(0x8, 0x00110001, titleID);
-    // printf("Changed menu to: %#018llx", *(u64*(titleID)));
-    // printf("Current menu: %s menu", isHomeMenu ? "test" : "home");
 
     hidScanInput();
     if (!hidKeysHeld()) APT_HardwareResetAsync();
